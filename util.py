b6289beb86d792068011a6abeac65571b47f938b
Matt Nicholson <nicholson.matt94@gmail.com>
2025-05-13 19:59:51 -0400

    Update util.py

--- util.py
+++ util.py
@@ -1,35 +1,32 @@
 from functools import wraps
 
 
-def round_return(ndigits: int | None = None):
+def round_return(decimals=2):
     """Decorator to round the return value of a function.
 
     Parameters
     ----------
     ndigits: int, optional
-        Number of digits to round to. Default is None.
+        Number of digits to round to. Default is 2.
     """
     def decorator(func):
-        @wraps(func)
+        @functools.wraps(func)
         def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
-            if isinstance(result, (int, float)):
-                return round(result, ndigits)
-            elif isinstance(result, tuple):
-                return tuple(
-                    round(x, ndigits)
-                    if isinstance(x, (int, float))
-                    else x for x in result)
-            elif isinstance(result, list):
-                return [
-                    round(x, ndigits)
-                    if isinstance(x, (int, float))
-                    else x for x in result]
-            elif isinstance(result, dict):
-                 return {
-                    k: (round(v, ndigits)
-                    if isinstance(v, (int, float))
-                    else v) for k, v in result.items()}
-            return result
+
+            def round_value(val):
+                if isinstance(val, float):
+                    return round(val, decimals)
+                elif isinstance(val, int):
+                    return val
+                elif isinstance(val, tuple):
+                    return tuple(round_value(x) for x in val)
+                elif isinstance(val, list):
+                    # Check if this is a list of tuples
+                    if all(isinstance(item, tuple) for item in val):
+                        return [round_value(item) for item in val]
+                return val
+
+            return round_value(result)
         return wrapper
     return decorator
